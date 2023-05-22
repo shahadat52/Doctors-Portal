4411d4c17ec3005f5c173d476736ab9aca067b02
shahadat52 <shahadathossain.sh255@gmail.com>
2023-05-22 22:53:53 +0600

    service sectin complete

--- src/Pages/Home/Services/Service.jsx
+++ src/Pages/Home/Services/Service.jsx
@@ -7,9 +7,9 @@ const Service = ({ service }) => {
         <div className="mx-auto card w-96 bg-base-100 shadow-xl text-black text-center">
             <figure><img className='mt-11' src={icon} alt="images" /></figure>
             <div className="card-body">
-                <h2 className="">{name}</h2>
+                <h2 className="text-[20px] font-[600]">{name}</h2>
                 <p className=''>{description}</p>
-                
+
             </div>
         </div>
     );
